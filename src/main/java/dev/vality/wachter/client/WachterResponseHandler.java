cad7d2fef4e88530dab63dda5ab5a56cd1ffe6e4
malkoas <alien.life.form.1986@gmail.com>
2022-11-02 17:57:50 +0300

    fix tests

--- src/main/java/dev/vality/wachter/client/WachterResponseHandler.java
+++ src/main/java/dev/vality/wachter/client/WachterResponseHandler.java
@@ -1,11 +1,9 @@
 package dev.vality.wachter.client;
 
 import dev.vality.woody.api.flow.error.WUnavailableResultException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.util.EntityUtils;
 import org.springframework.http.HttpStatus;
