a3fe3d0d43c57d701bead9c45efed27fdbbe5f40
malkoas <alien.life.form.1986@gmail.com>
2022-11-02 17:25:11 +0300

    Add response handler

--- src/main/java/dev/vality/wachter/client/WachterClient.java
+++ src/main/java/dev/vality/wachter/client/WachterClient.java
@@ -1,16 +1,12 @@
 package dev.vality.wachter.client;
 
-import dev.vality.woody.api.flow.error.WUnavailableResultException;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.util.EntityUtils;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,6 +18,7 @@ import java.util.Enumeration;
 public class WachterClient {
 
     private final HttpClient httpclient;
+    private final WachterResponseHandler responseHandler;
 
     @SneakyThrows
     public byte[] send(HttpServletRequest request, byte[] contentData, String url) {
@@ -29,20 +26,9 @@ public class WachterClient {
         setHeader(request, httppost);
         httppost.setEntity(new ByteArrayEntity(contentData));
         log.info("Send request to url {}", url);
-        HttpResponse response = httpclient.execute(httppost);
+        var response = httpclient.execute(httppost, responseHandler);
         log.info("Get response {}", response);
-        HttpEntity httpEntity = response.getEntity();
-        int statusCode = response.getStatusLine().getStatusCode();
-        HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
-        if (httpStatus.is2xxSuccessful()) {
-            return EntityUtils.toByteArray(httpEntity);
-        } else if (httpStatus.is5xxServerError()) {
-            throw new WUnavailableResultException(String.format("Received 5xx error code: %s (response: %s)",
-                    httpStatus, response));
-        } else {
-            throw new RuntimeException(String.format("Wrong status was received: %s (response: %s)",
-                    httpStatus, response));
-        }
+        return response;
     }
 
     private void setHeader(HttpServletRequest request, HttpPost httppost) {
---
+++ src/main/java/dev/vality/wachter/client/WachterResponseHandler.java
@@ -0,0 +1,33 @@
+package dev.vality.wachter.client;
+
+import dev.vality.woody.api.flow.error.WUnavailableResultException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.util.EntityUtils;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+
+@Slf4j
+public class WachterResponseHandler implements ResponseHandler<byte[]> {
+
+    @Override
+    public byte[] handleResponse(HttpResponse httpResponse) throws IOException {
+        int statusCode = httpResponse.getStatusLine().getStatusCode();
+        HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
+        if (httpStatus.is2xxSuccessful()) {
+            HttpEntity httpEntity = httpResponse.getEntity();
+            return EntityUtils.toByteArray(httpEntity);
+        } else if (httpStatus.is5xxServerError()) {
+            throw new WUnavailableResultException(String.format("Received 5xx error code: %s (response: %s)",
+                    httpStatus, httpResponse));
+        } else {
+            throw new RuntimeException(String.format("Wrong status was received: %s (response: %s)",
+                    httpStatus, httpResponse));
+        }
+    }
+}
