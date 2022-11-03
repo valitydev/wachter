bd2e04ef75f07a18125aac8d1fd2801f9ed564da
malkoas <41993717+malkoas@users.noreply.github.com>
2022-11-03 11:28:41 +0300

    fix/add PoolingHttpClientConnectionManager (#26)

---
+++ src/main/java/dev/vality/wachter/config/properties/HttpClientProperties.java
@@ -0,0 +1,33 @@
+package dev.vality.wachter.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Validated
+@Configuration
+@ConfigurationProperties("http-client")
+public class HttpClientProperties {
+
+    @NotNull
+    private int maxTotalPooling;
+
+    @NotNull
+    private int defaultMaxPerRoute;
+
+    @NotNull
+    private int socketTimeout;
+
+    @NotNull
+    private int connectionRequestTimeout;
+
+    @NotNull
+    private int connectTimeout;
+
+}
