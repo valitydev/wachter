8c95225a5a71675d1b4100ba51e5516fe867b0c4
Anatoly Karlov <karleowne@gmail.com>
2025-10-07 16:49:04 +0700

    http1 with apache http 5 config

--- src/main/java/dev/vality/wachter/config/ApplicationConfig.java
+++ src/main/java/dev/vality/wachter/config/ApplicationConfig.java
@@ -2,30 +2,12 @@ package dev.vality.wachter.config;
 
 import dev.vality.wachter.client.WachterClient;
 import dev.vality.wachter.client.WachterRequestFactory;
-import dev.vality.wachter.config.properties.HttpClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
-
-import java.net.http.HttpClient;
-import java.time.Duration;
 
 @Configuration
 public class ApplicationConfig {
-
-    @Bean
-    public RestClient restClient(RestClient.Builder builder, HttpClientProperties properties) {
-        var connectTimeout = Duration.ofMillis(properties.getConnectTimeout());
-        var httpClient = HttpClient.newBuilder()
-                .connectTimeout(connectTimeout)
-                .build();
-
-        var requestFactory = new JdkClientHttpRequestFactory(httpClient);
-        requestFactory.setReadTimeout(Duration.ofMillis(properties.getSocketTimeout()));
-
-        return builder.requestFactory(requestFactory).build();
-    }
 
     @Bean
     public WachterClient wachterClient(RestClient restClient, WachterRequestFactory requestFactory) {
---
+++ src/main/java/dev/vality/wachter/config/RestClientConfig.java
@@ -0,0 +1,114 @@
+package dev.vality.wachter.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.vality.wachter.config.properties.HttpProperties;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.client5.http.ssl.HostnameVerificationPolicy;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.TrustAllStrategy;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.io.SocketConfig;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestClient;
+
+import javax.net.ssl.SSLContext;
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+public class RestClientConfig {
+
+    private final HttpProperties httpProperties;
+
+    public RestClientConfig(HttpProperties httpProperties) {
+        this.httpProperties = httpProperties;
+    }
+
+    @Bean
+    public SSLContext sslContext() throws Exception {
+        return SSLContextBuilder.create()
+                .loadTrustMaterial(null, TrustAllStrategy.INSTANCE)
+                .build();
+    }
+
+    @Bean
+    public PoolingHttpClientConnectionManager connectionManager(SSLContext sslContext) {
+        return PoolingHttpClientConnectionManagerBuilder.create()
+                .setTlsSocketStrategy(new DefaultClientTlsStrategy(
+                        sslContext,
+                        HostnameVerificationPolicy.CLIENT,
+                        NoopHostnameVerifier.INSTANCE
+                ))
+                .setDefaultSocketConfig(SocketConfig.custom()
+                        .setSoTimeout(Timeout.ofMilliseconds(httpProperties.getRequestTimeout()))
+                        .build())
+                .setDefaultConnectionConfig(ConnectionConfig.custom()
+                        .setConnectTimeout(Timeout.ofMilliseconds(httpProperties.getConnectionTimeout()))
+                        .setSocketTimeout(Timeout.ofMilliseconds(httpProperties.getRequestTimeout()))
+                        .build())
+                .setMaxConnTotal(httpProperties.getMaxTotalPooling())
+                .setMaxConnPerRoute(httpProperties.getDefaultMaxPerRoute())
+                .build();
+    }
+
+    @Bean
+    public RequestConfig requestConfig() {
+        return RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(httpProperties.getPoolTimeout()))
+                .setResponseTimeout(Timeout.ofMilliseconds(httpProperties.getRequestTimeout()))
+                .build();
+    }
+
+    @Bean
+    public CloseableHttpClient httpClient(
+            PoolingHttpClientConnectionManager connectionManager,
+            RequestConfig requestConfig) {
+        return HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .disableRedirectHandling()
+                .disableAutomaticRetries()
+                .addRequestInterceptorLast((request, entity, context) ->
+                        request.removeHeaders(HttpHeaders.CONNECTION))
+                .setConnectionManagerShared(true)
+                .build();
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory requestFactory(HttpClient httpClient) {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        factory.setConnectTimeout(Duration.ofMillis(httpProperties.getConnectionTimeout()));
+        factory.setConnectionRequestTimeout(Duration.ofMillis(httpProperties.getPoolTimeout()));
+        factory.setReadTimeout(Duration.ofMillis(httpProperties.getRequestTimeout()));
+        return factory;
+    }
+
+    @Bean
+    public RestClient restClient(ClientHttpRequestFactory requestFactory, ObjectMapper objectMapper) {
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .messageConverters(converters -> updateObjectMapper(converters, objectMapper))
+                .build();
+    }
+
+    private void updateObjectMapper(List<HttpMessageConverter<?>> converters, ObjectMapper objectMapper) {
+        converters.stream()
+                .filter(MappingJackson2HttpMessageConverter.class::isInstance)
+                .map(MappingJackson2HttpMessageConverter.class::cast)
+                .forEach(converter -> converter.setObjectMapper(objectMapper));
+    }
+}
